a29e82e86c8855b161772147d5b725c29c5afc60
EricChiou <miatlab@gmail.com>
2021-02-07 16:57:14 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+window.onload = () => {
+  const search = window.location.search;
+  if (search) {
+    const keyValueStrAry = search.slice(1).split('&');
+    for (const keyValueStr of keyValueStrAry) {
+      const keyValue = keyValueStr.split('=');
+      if (keyValue.length === 2 && keyValue[0] === 'style') {
+        try {
+          const style = JSON.parse(keyValue[1]);
+          document.body.style = style;
+        } catch (error) { }
+        break;
+      }
+    }
+  }
+
+  const index = Math.ceil(Math.random() * 10);
+  document.getElementById('img').src = `./image/${index}.jpg`;
+};
